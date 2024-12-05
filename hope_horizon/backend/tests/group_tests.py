0b76dcd91d9b5e0042a243c8a6e63d9240509f04
KamKnez <kamilo.knezevic@edu.fh-joanneum.at>
2024-12-05 16:24:08 +0100

    group-tests-finished

--- hope_horizon/backend/tests/group_tests.py
+++ hope_horizon/backend/tests/group_tests.py
@@ -9,7 +9,7 @@ from backend.serializers.group_serializer import GroupSerializer
 class GroupTests(APITestCase):
 
     def setUp(self):
-        # Create test user
+        # Create test users
         self.user1 = User.objects.create_user(
             username="testuser1",
             password="testpassword",
@@ -24,10 +24,22 @@ class GroupTests(APITestCase):
             user_role_id=UserRole.objects.get(role="User"),
         )
 
+        self.user3 = User.objects.create_user(
+            username="testuser3",
+            password="testpassword",
+            birthdate=Date(1990, 1, 1),
+            user_role_id=UserRole.objects.get(role="User"),
+        )
+
         # Create test group
         self.test_group = CustomGroup.objects.create(
             name="testgroup",
             description="testdescription"
+        )
+        self.test_group_inactive = CustomGroup.objects.create(
+            name="testgroup2",
+            description="testdescription",
+            is_active=False
         )
         
         # Create test group user owner
@@ -48,7 +60,6 @@ class GroupTests(APITestCase):
 
         # Login the test users
         self.client.login(username="testuser1", password="testpassword")
-        self.client.login(username="testuser2", password="testpassword")
 
     ###########################################################################################################
     #                                Group "GET(list)" method tests                                     #
@@ -92,13 +103,137 @@ class GroupTests(APITestCase):
     # test if user is not authenticated
     def test_group_retrieve_not_authenticated(self):
         self.client.logout()
-        response = self.client.get("/api/group/1", follow=True)
+        response = self.client.get("/api/group/1/", follow=True)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     # test when group does not exist
     def test_group_retrieve_group_not_exist(self):
-        response = self.client.get("/api/group/10", follow=True)
+        response = self.client.get("/api/group/10/", follow=True)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    # test when group is inactive
+    def test_group_retrieve_group_inactive(self):
+        response = self.client.get("/api/group/2/", follow=True)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    # test when user is not authorized
+    def test_group_retrieve_user_not_authorized(self):
+        self.client.logout()
+        self.client.login(username="testuser3", password="testpassword")
+        response = self.client.get("/api/group/1/", follow=True)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    # test when user is authorized
+    def test_group_retrieve_user_authorized(self):
+        response = self.client.get("/api/group/1/", follow=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self._validate_group(response.data)
+
+    ###########################################################################################################
+    #                                Group "POST(create)" method tests                                     #
+    ###########################################################################################################
+
+    # test if user is not authenticated
+    def test_group_create_not_authenticated(self):
+        self.client.logout()
+        response = self.client.post("/api/group/", {"name": "testgroup3", "description": "testdescription"}, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    # test when name is not given
+    def test_group_create_no_name(self):
+        response = self.client.post("/api/group/", {"description": "testdescription"}, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    # test when description is not given
+    def test_group_create_no_description(self):
+        response = self.client.post("/api/group/", {"name": "testgroup3"}, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    # test when group name already exists
+    def test_group_create_group_name_exists(self):
+        response = self.client.post("/api/group/", {"name": "testgroup", "description": "testdescription"}, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # test when group is created successfully
+    def test_group_create_success(self):
+        response = self.client.post("/api/group/", {"name": "testgroup3", "description": "testdescription"}, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self._validate_group(response.data, expected_name="testgroup3", expected_description="testdescription")
+
+    ###########################################################################################################
+    #                                Group "PUT(update)" method tests                                     #
+    ###########################################################################################################
+
+    # test if user is not authenticated
+    def test_group_update_not_authenticated(self):
+        self.client.logout()
+        response = self.client.put("/api/group/1/", {"name": "testgroup3", "description": "testdescription"}, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    # test when name is not given
+    def test_group_update_no_name(self):
+        response = self.client.put("/api/group/1/", {"description": "testdescription"}, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    # test when description is not given
+    def test_group_update_no_description(self):
+        response = self.client.put("/api/group/1/", {"name": "testgroup3"}, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # test when group does not exist
+    def test_group_update_group_not_exist(self):
+        response = self.client.put("/api/group/10/", {"name": "testgroup3", "description": "testdescription"}, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    # test when group is inactive
+    def test_group_update_group_inactive(self):
+        response = self.client.put("/api/group/2/", {"name": "testgroup3", "description": "testdescription"}, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    # test when user is not authorized
+    def test_group_update_user_not_authorized(self):
+        self.client.logout()
+        self.client.login(username="testuser3", password="testpassword")
+        response = self.client.put("/api/group/1/", {"name": "testgroup3", "description": "testdescription"}, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    # test when user is authorized
+    def test_group_update_user_authorized(self):
+        response = self.client.put(f"/api/group/1/", {"name": "testgroup3", "description": "testdescription"}, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self._validate_group(response.data, expected_name="testgroup3", expected_description="testdescription")
+
+    ###########################################################################################################
+    #                                Group "PUT(update)" method tests                                     #
+    ###########################################################################################################
+
+    # test if user is not authenticated
+    def test_group_delete_not_authenticated(self):
+        self.client.logout()
+        response = self.client.delete("/api/group/1/", follow=True)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    # test when group does not exist
+    def test_group_delete_group_not_exist(self):
+        response = self.client.delete("/api/group/10/", follow=True)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    # test when group is inactive
+    def test_group_delete_group_inactive(self):
+        response = self.client.delete("/api/group/2/", follow=True)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    # test when user is not authorized
+    def test_group_delete_user_not_authorized(self):
+        self.client.logout()
+        self.client.login(username="testuser3", password="testpassword")
+        response = self.client.delete("/api/group/1/", follow=True)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    # test when user is authorized
+    def test_group_delete_user_authorized(self):
+        response = self.client.delete("/api/group/1/", follow=True)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
     
 
     ###########################################################################################################
@@ -111,5 +246,13 @@ class GroupTests(APITestCase):
             self.assertEqual(group["name"], self.test_group.name)
             self.assertEqual(group["description"], self.test_group.description)
 
-
-    
+    def _validate_group(self, response, expected_name=None, expected_description=None):
+        self.assertGreaterEqual(response["id"], 1)
+        if expected_name:
+            self.assertEqual(response["name"], expected_name)
+        else:
+            self.assertEqual(response["name"], self.test_group.name)
+        if expected_description:
+            self.assertEqual(response["description"], expected_description)
+        else:
+            self.assertEqual(response["description"], self.test_group.description)
--- hope_horizon/backend/views/group_view.py
+++ hope_horizon/backend/views/group_view.py
@@ -29,9 +29,12 @@ class GroupViewSet(viewsets.ModelViewSet):
             group = CustomGroup.objects.get(pk=pk)
         except CustomGroup.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        if CustomGroup.objects.filter(pk=pk).filter(is_active=False):
+        if group.is_active == False:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        if user != GroupUser.objects.filter(user=user).filter(group=group).filter(is_active=True).user:
+        #check if the user is a member of the group and is active
+        try:
+            GroupUser.objects.filter(user=user).filter(group=group).filter(is_active=True).get()
+        except GroupUser.DoesNotExist:
             return Response(status=status.HTTP_403_FORBIDDEN) 
         serializer = self.serializer_class(group)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -47,35 +50,42 @@ class GroupViewSet(viewsets.ModelViewSet):
             return Response("group name already exits", status=status.HTTP_400_BAD_REQUEST)
         group = CustomGroup.objects.create(name=request.data.get("name"), description=request.data.get("description"))
         GroupUser.objects.create(user=request.user, group=group, is_owner=True, is_active=True)
-        serializer.save(id=group.pk)
+        serializer = self.serializer_class(group)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     def update(self, request, pk=None):
         user = request.user
-        serializer = self.get_serializer(data=request.data)
-        if request.data.get("name") is None or request.data.get("description") is None or pk is None:
+        name = request.data.get("name")
+        description = request.data.get("description")
+        
+        if name is None or description is None:
             return Response("Invalid request data", status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid()
-        group = CustomGroup.objects.get(pk=pk)
-        if group is None or group.is_active == False:
+        try:
+            group = CustomGroup.objects.get(pk=pk)
+        except CustomGroup.DoesNotExist:
             return Response("Group not found", status=status.HTTP_404_NOT_FOUND)
-        group_user = GroupUser.objects.filter(user=user).filter(group=group).filter(is_active=True).filter(owner=True)
+        if group.is_active == False:
+            return Response("Group not found", status=status.HTTP_404_NOT_FOUND)
+        group_user = GroupUser.objects.filter(user=user).filter(group=group).filter(is_active=True).filter(is_owner=True)
         if not group_user.exists():
             return Response("User not authorized", status=status.HTTP_403_FORBIDDEN)
-        group.name = request.data.get("name")
-        group.description = request.data.get("description")
+        group.name = name
+        group.description = description
         group.save()
-        serializer.save(group_id=group)
+        serializer = self.serializer_class(group)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def destroy(self, request, pk=None):
         user = request.user
-        if pk is None:
-            return Response("Invalid request data", status=status.HTTP_400_BAD_REQUEST)
-        group = CustomGroup.objects.get(pk=pk)
-        if group.DoesNotExist or group.is_active == False:
+        try:
+            group = CustomGroup.objects.get(pk=pk)
+        except CustomGroup.DoesNotExist:
             return Response("Group not found", status=status.HTTP_404_NOT_FOUND)
-        group_user = GroupUser.objects.filter(user=user).filter(group=group).filter(is_active=True).filter(owner=True)
+        if group.is_active == False:
+            return Response("Group not found", status=status.HTTP_404_NOT_FOUND)
+        group_user = GroupUser.objects.filter(user=user).filter(group=group).filter(is_active=True).filter(is_owner=True)
         if not group_user.exists():
             return Response("User not authorized", status=status.HTTP_403_FORBIDDEN)
         group.is_active = False
