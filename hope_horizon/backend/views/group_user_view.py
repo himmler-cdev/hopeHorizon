0e161e9b41028cbe2c99edc46ff380da60b257ce
KamKnez <kamilo.knezevic@edu.fh-joanneum.at>
2024-12-05 16:53:01 +0100

    group-user-tests

--- hope_horizon/backend/views/group_user_view.py
+++ hope_horizon/backend/views/group_user_view.py
@@ -9,21 +9,23 @@ class GroupUserViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class_group_user = GroupUserSerializer
 
-    def list(self, request, pk=None):
+    def list(self, request):
         user = request.user
+        id = request.query_params.get("group_id", None)
         try:
-            group = CustomGroup.objects.get(pk=pk)
+            group = CustomGroup.objects.get(pk=id)
         except CustomGroup.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         if not group.is_active:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-        group_user_owner = GroupUser.objects.filter(group=group).filter(is_active=True).filter(user=user).filter(owner=True)
-        if not group_user_owner.exists():
+        try:
+            group_user = GroupUser.objects.filter(group=group).filter(user=user).get()
+            if not group_user.is_owner:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+        except GroupUser.DoesNotExist:
             return Response(status=status.HTTP_403_FORBIDDEN)
         
-        group_user_list = GroupUser.objects.filter(group=group).filter(is_active=True).filter(owner=False)
+        group_user_list = GroupUser.objects.filter(group=group).filter(is_active=True).filter(is_owner=False)
         serializer = self.serializer_class_group_user(group_user_list, many=True)
         return Response({"group_users": serializer.data}, status=status.HTTP_200_OK)
 
