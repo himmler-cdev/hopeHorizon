3aee388589c467ac32a4b99bc74cef93dedf3562
KamKnez <162553493+KamKnez@users.noreply.github.com>
2024-12-10 12:19:01 +0100

    Revert "feat(27): group backend (#32)"
    This reverts commit 57b32694189af30876396055ec6b3173f3b725aa.

--- hope_horizon/backend/tests/__init__.py
+++ hope_horizon/backend/tests/__init__.py
@@ -3,5 +3,3 @@ from .user_status_tests import UserStatusTests
 from .user_tracker_tests import UserTrackerTests
 from .blog_post_type_tests import BlogPostTypeTests
 from .blog_post_tests import BlogPostTests
-from .group_tests import GroupTests
-from .group_user_tests import GroupUserTests
