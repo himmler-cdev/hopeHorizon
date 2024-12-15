cdc1072eba2004e442ebebe6033b4ddaa09aae49
himmler-christoph <christoph.himmler@edu.fh-joanneum.at>
2024-12-15 19:12:05 +0100

    feat(31): blog post page

---
+++ hope_horizon_frontend/src/app/feature-blog/blog-list-edit/blog-list-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-list-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-list-edit.component.html',
+  styleUrl: './blog-list-edit.component.scss'
+})
+export class BlogListEditComponent {
+
+}
