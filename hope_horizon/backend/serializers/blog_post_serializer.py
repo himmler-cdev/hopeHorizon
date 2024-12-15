8b9579d9c60e6b070c3ae8cb974baef574459b2a
himmler-christoph <94561124+himmler-cdev@users.noreply.github.com>
2024-12-15 16:25:22 +0100

    feat(30): blog post groups backend (#36)

--- hope_horizon/backend/serializers/blog_post_serializer.py
+++ hope_horizon/backend/serializers/blog_post_serializer.py
@@ -5,13 +5,13 @@ from backend.models import BlogPost
 class BlogPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
-        fields = ["id", "date", "title", "content", "blog_post_type_id"]
+        fields = ["id", "date", "title", "content", "blog_post_type_id", "group_id"]
         extra_kwargs = {
             "title": {"required": True},
             "content": {"required": True},
             "blog_post_type_id": {"required": True}
         }
-        read_only_fields = ["id", "date"]
+        read_only_fields = ["id", "date", "group_id"]
 
 class BlogPostListSerializer(serializers.Serializer):
     page_information = serializers.SerializerMethodField()
@@ -20,7 +20,7 @@ class BlogPostListSerializer(serializers.Serializer):
     def get_page_information(self, obj):
         return {
             "page": obj.get('page', 1),
-            "pageSize": obj.get('pageSize', 10),
+            "page_size": obj.get('page_size', 10),
             "actualSize": len(obj.get('blog_posts', []))
         }
 
--- hope_horizon/backend/views/blog_post_view_set.py
+++ hope_horizon/backend/views/blog_post_view_set.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from backend.models import BlogPost, BlogPostType, UserRole
+from backend.models import BlogPost, BlogPostType, UserRole, GroupUser, CustomGroup
 from backend.serializers import BlogPostSerializer, BlogPostListSerializer
 
 
@@ -17,70 +17,79 @@ class BlogPostViewSet(viewsets.ModelViewSet):
         # Query parameters
         try: 
             page = request.GET.get('page', None)
-            pageSize = int(request.GET.get('pageSize', 10))
+            page_size = int(request.GET.get('page_size', 10))
             if page is None:
                 return Response({"detail": "Page parameter is required"}, status=status.HTTP_400_BAD_REQUEST)
             page = int(page)
         except ValueError:
-            return Response({"detail": "PageSize must be an integer"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"detail": "page_size must be an integer"}, status=status.HTTP_400_BAD_REQUEST)
                        
         search = request.GET.get('search', '')
         blog_post_type_id = request.GET.get('blog_post_type_id', None)
         owned = request.GET.get('owned', None)
         workspace = request.GET.get('workspace', None)
+        group_name = request.GET.get('group_name', None)
 
         # Pagination
         if page < 1:
             return Response({"detail": "Page must be >= 1"}, status=status.HTTP_400_BAD_REQUEST)
-        if pageSize < 1:
-            return Response({"detail": "PageSize must be >= 1"}, status=status.HTTP_400_BAD_REQUEST)
+        if page_size < 1:
+            return Response({"detail": "page_size must be >= 1"}, status=status.HTTP_400_BAD_REQUEST)
 
         # Filtering
         if blog_post_type_id:
             try:
+                blog_post_type_id = int(blog_post_type_id)
                 BlogPostType.objects.get(id=blog_post_type_id)
+            except ValueError:
+                return Response({"detail": "Invalid blog post type id"}, status=status.HTTP_400_BAD_REQUEST)
             except BlogPostType.DoesNotExist:
                 return Response({"detail": "Invalid blog post type id"}, status=status.HTTP_400_BAD_REQUEST)
-            blogPosts = BlogPost.objects.filter(title__icontains=search, blog_post_type_id=blog_post_type_id)
+            blog_posts = BlogPost.objects.filter(title__icontains=search, blog_post_type_id=blog_post_type_id)
         else:
-            blogPosts = BlogPost.objects.filter(title__icontains=search)
+            blog_posts = BlogPost.objects.filter(title__icontains=search)
 
         # Owned, therapist and workspace filtering
         if workspace is not None and workspace.lower() not in ['true', 'false']:
             return Response({"detail": "Invalid value for workspace parameter"}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Therapist and workspace filtering
-        if request.user.user_role_id == UserRole.objects.get(role="Therapist") and workspace is not None and workspace.lower() == 'true':
-            # Therapist handling
-            protected_blog_post_type = BlogPostType.objects.get(type="Protected")
-            public_blog_post_type = BlogPostType.objects.get(type="Public")
-            blogPosts = blogPosts.filter(blog_post_type_id__in=[protected_blog_post_type.id, public_blog_post_type.id])
-            blogPosts = blogPosts.order_by('-blog_post_type_id', 'date')
-        # If user is not a therapist
-        else:
+        # Group filtering
+        if blog_post_type_id == BlogPostType.objects.get(type="Group").id:
+            if group_name is not None:
+                group = CustomGroup.objects.filter(name__icontains=group_name).first()
+                if group and GroupUser.objects.filter(group_id=group.id, user_id=request.user.id, is_active=True).exists():
+                    blog_posts = blog_posts.filter(group_id=group.id)
+                else:
+                    return Response({"detail": "You are not a member of the requested group or the group does not exist"}, status=status.HTTP_403_FORBIDDEN)
+            else:
+                group_ids = GroupUser.objects.filter(user_id=request.user.id, is_active=True).values_list('group_id', flat=True)
+                blog_posts = blog_posts.filter(group_id__in=group_ids)
+        # Check if user is a therapist
+        elif request.user.user_role_id == UserRole.objects.get(role="Therapist"):
+            blog_posts = self._filter_for_therapist(blog_posts, workspace)
+        # Check if user is not a therapist and owned parameter is provided
+        else: 
             if owned is not None:
                 if owned.lower() == 'true':
-                    blogPosts = blogPosts.filter(user_id=request.user.id)
+                    blog_posts = blog_posts.filter(user_id=request.user.id)
                 elif owned.lower() == 'false':
                     public_blog_post_type = BlogPostType.objects.get(type="Public")
-                    blogPosts = blogPosts.exclude(user_id=request.user.id).filter(blog_post_type_id=public_blog_post_type.id)
+                    blog_posts = blog_posts.exclude(user_id=request.user.id).filter(blog_post_type_id=public_blog_post_type.id)
                 else:
                     return Response({"detail": "Invalid value for owned parameter"}, status=status.HTTP_400_BAD_REQUEST)
             else:
-                return Response({"detail": "Owned parameter is required"}, status=status.HTTP_400_BAD_REQUEST)
-            # TODO: Order by most commented
-
-        offset = (page - 1) * pageSize
-        blogPosts = blogPosts[offset:offset + pageSize]
-        totalPosts = blogPosts.count()
-
-        # TODO: Add group_name filtering and groups in general
+                return Response({"detail": "Owned parameter is required for non-therapist users"}, status=status.HTTP_400_BAD_REQUEST)
+            blog_posts = blog_posts.order_by('-date')
+        
+        offset = (page - 1) * page_size
+        blog_posts = blog_posts[offset:offset + page_size]
+        totalPosts = blog_posts.count()
 
         data = {
             "page": page,
-            "pageSize": pageSize,
+            "page_size": page_size,
             "actualSize": totalPosts,
-            "blog_posts": blogPosts
+            "blog_posts": blog_posts
         }
 
         serializer = self.serializers["list"](data)
@@ -92,9 +101,19 @@ class BlogPostViewSet(viewsets.ModelViewSet):
         except BlogPost.DoesNotExist:
             return Response({"detail": "Blog post not found"}, status=status.HTTP_404_NOT_FOUND)
         
-        # TODO: Check if user is in group of the blog_post
-        if (blog_post.user_id == request.user or blog_post.blog_post_type_id == BlogPostType.objects.get(type="Public") or \
-            request.user.user_role_id == UserRole.objects.get(role="Therapist") and blog_post.blog_post_type_id == BlogPostType.objects.get(type="Protected")):
+        # Checks if user is the owner of the blog_post or if the blog_post is public
+        if blog_post.user_id == request.user or blog_post.blog_post_type_id == BlogPostType.objects.get(type="Public"):
+            serializer = self.serializers["default"](blog_post)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        # Checks if the user is a therapist and the blog_post is protected
+        if request.user.user_role_id == UserRole.objects.get(role="Therapist") and blog_post.blog_post_type_id == BlogPostType.objects.get(type="Protected"):
+            serializer = self.serializers["default"](blog_post)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        # Check if user is in group of the blog_post
+        group_users = GroupUser.objects.filter(group_id=blog_post.group_id, user_id=request.user.id, is_active=True)
+        if blog_post.blog_post_type_id == BlogPostType.objects.get(type="Group") and group_users.exists():
             serializer = self.serializers["default"](blog_post)
             return Response(serializer.data, status=status.HTTP_200_OK)
     
@@ -104,7 +123,17 @@ class BlogPostViewSet(viewsets.ModelViewSet):
         serializer = self.serializers["default"](data=request.data)
         if not serializer.is_valid():
             return Response({"detail": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-        serializer.save(user_id=request.user)
+
+        # Check if user tries to create a group blog post
+        if request.data.get('blog_post_type_id', None) == BlogPostType.objects.get(type="Group").id:
+            try:
+                CustomGroup.objects.get(id=request.data.get('group_id'))
+            except CustomGroup.DoesNotExist:
+                return Response({"detail": "Invalid group id"}, status=status.HTTP_400_BAD_REQUEST)
+            serializer.save(user_id=request.user, group_id_id=request.data.get('group_id'))
+        else:
+            serializer.save(user_id=request.user, group_id=None)
+
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def update(self, request, pk):
@@ -120,7 +149,16 @@ class BlogPostViewSet(viewsets.ModelViewSet):
         if not serializer.is_valid():
             return Response({"detail": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         
-        serializer.save()
+        # Check if user tries to create a group blog post
+        if request.data.get('blog_post_type_id', None) == BlogPostType.objects.get(type="Group").id:
+            try:
+                CustomGroup.objects.get(id=request.data.get('group_id'))
+            except CustomGroup.DoesNotExist:
+                return Response({"detail": "Invalid group id"}, status=status.HTTP_400_BAD_REQUEST)
+            serializer.save(group_id_id=request.data.get('group_id'))
+        else:
+            serializer.save(group_id=None)
+
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def destroy(self, request, pk):
@@ -134,3 +172,16 @@ class BlogPostViewSet(viewsets.ModelViewSet):
         
         self.perform_destroy(blog_post)
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # Helper functions for list
+    def _filter_for_therapist(self, blog_posts, workspace):
+        if workspace is not None and workspace.lower() == 'true':
+            protected_blog_post_type = BlogPostType.objects.get(type="Protected")
+            public_blog_post_type = BlogPostType.objects.get(type="Public")
+            blog_posts = blog_posts.filter(blog_post_type_id__in=[protected_blog_post_type.id, public_blog_post_type.id])
+            blog_posts = blog_posts.order_by('-blog_post_type_id', 'date')
+        else:
+            public_blog_post_type = BlogPostType.objects.get(type="Public")
+            blog_posts = blog_posts.filter(blog_post_type_id=public_blog_post_type.id)
+
+        return blog_posts
