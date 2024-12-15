bf533c782965f18e946c7a961de6769654d04776
Wild Stefan <stefan.wild3@edu.fh-joanneum.at>
2024-12-15 19:06:12 +0100

    finished design for login and registration

--- hope_horizon_frontend/src/app/app.routes.ts
+++ hope_horizon_frontend/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import {Routes} from '@angular/router';
 import {BlogListComponent} from './feature-blog/blog-list/blog-list.component';
+import { UserLoginComponent } from './feature-user/user-login/user-login.component';
+import { UserRegisterComponent } from './feature-user/user-register/user-register.component';
 
 export const routes: Routes = [
-  {path: 'blog-list', component: BlogListComponent}
+  {path: 'blog-list', component: BlogListComponent},
+  {path: 'login', component: UserLoginComponent},
+  {path: 'register', component: UserRegisterComponent},
 ];
