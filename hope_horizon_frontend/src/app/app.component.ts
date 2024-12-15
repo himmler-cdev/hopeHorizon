bcbc454ddaaabd4e1fe783a52be34d85f2060ed1
himmler-christoph <christoph.himmler@edu.fh-joanneum.at>
2024-12-15 16:59:43 +0100

    spacing

--- hope_horizon_frontend/src/app/app.component.ts
+++ hope_horizon_frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {MatButton} from '@angular/material/button';
 import {HeaderComponent} from './feature-nav/header/header.component';
 import {FooterComponent} from './feature-nav/footer/footer.component';
