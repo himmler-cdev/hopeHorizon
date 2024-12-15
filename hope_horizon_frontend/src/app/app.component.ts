02046389bec2c6770a6b8ef64457ed845c80e3ad
himmler-christoph <94561124+himmler-cdev@users.noreply.github.com>
2024-12-15 17:00:41 +0100

    feat(31): implemented header and footer (#38)
    * feat(31): implemented header and footer
    
    * spacing

--- hope_horizon_frontend/src/app/app.component.ts
+++ hope_horizon_frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {MatButton} from '@angular/material/button';
+import {HeaderComponent} from './feature-nav/header/header.component';
+import {FooterComponent} from './feature-nav/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButton],
+  imports: [RouterOutlet, MatButton, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- hope_horizon_frontend/src/app/app.routes.ts
+++ hope_horizon_frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {BlogListComponent} from './feature-blog/blog-list/blog-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'blog-list', component: BlogListComponent}
+];
