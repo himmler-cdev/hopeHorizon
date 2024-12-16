48c78b61a8971e0d829b4a0c8e3bd686b416e703
himmler-christoph <christoph.himmler@edu.fh-joanneum.at>
2024-12-16 23:30:04 +0100

    feat(31): code and style improvments

--- hope_horizon_frontend/src/app/feature-nav/header/header.component.ts
+++ hope_horizon_frontend/src/app/feature-nav/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
 import {MatAnchor, MatButton, MatIconButton} from '@angular/material/button';
