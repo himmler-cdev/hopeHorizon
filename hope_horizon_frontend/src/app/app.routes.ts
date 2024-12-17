446fae0f3c9897708004d3ff58b103d95cf7e00e
himmler-christoph <christoph.himmler@edu.fh-joanneum.at>
2024-12-17 20:25:46 +0100

    feat(31): style changes

--- hope_horizon_frontend/src/app/app.routes.ts
+++ hope_horizon_frontend/src/app/app.routes.ts
@@ -2,10 +2,19 @@ import {Routes} from '@angular/router';
 import {BlogFeedComponent} from './feature-blog/blog-feed/blog-feed.component';
 import {BlogJournalComponent} from './feature-blog/blog-journal/blog-journal.component';
 import {BlogFormComponent} from './feature-blog/blog-form/blog-form.component';
+import {BlogDetailComponent} from './feature-blog/blog-detail/blog-detail.component';
 
 export const routes: Routes = [
+  /**
+   * Blog routes
+   */
   {path: 'feed', component: BlogFeedComponent},
   {path: 'journal', component: BlogJournalComponent},
   {path: 'blog/create', component: BlogFormComponent},
-  {path: 'blog/edit/:id', component: BlogFormComponent}
+  {path: 'blog/edit/:id', component: BlogFormComponent},
+  {path: 'blog/:id', component: BlogDetailComponent}
+
+  /**
+   * TODO: YOUR ROUTES @Kamilo, @David
+   */
 ];
